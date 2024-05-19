1ffceb1c7cd0442c14afd53beea301180e6ab2ae
Nyamekye Korsah <nkorsah001@st.ug.edu.gh>
2024-05-19 12:08:35 +0000

    initial commit

---
+++ userInfo.js
@@ -0,0 +1,9 @@
+
+function createUserProfiles(names, modifiedNames) {
+    return names.map((name, index) => ({
+        originalName: name,
+        modifiedName: modifiedNames[index],
+        id: index + 1
+    }));
+}
+
